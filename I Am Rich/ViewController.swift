f4406dfc5176534626c74c24bff89a37abafc205
Dawid Tobor <dawidtobor@op.pl>
2018-11-26 23:32:57 +0100

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Dawid on 26/11/2018.
+//  Copyright © 2018 Ishiguro. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
